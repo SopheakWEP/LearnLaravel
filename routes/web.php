4c3c5ff3cc5ec71427930cf7a3eaf046ad20682d
SopheakWEP <sopheak.riths@gmail.com>
2018-11-22 23:02:33 +0700

    config app on link

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/login',function(){
+	return view('login');
+});
 Route::get('/', function () {
     return view('index');
 });
